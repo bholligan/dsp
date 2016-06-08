e714e8356ed2751548f39a78fe6c6b807a773e8f
Brian Holligan <brian.matthew.holligan@gmail.com>
2016-06-08 04:20:04 -0500

    Update q8_parsing.py
    Completed.

--- python/q8_parsing.py
+++ python/q8_parsing.py
@@ -4,3 +4,16 @@
 # against opponents, and had 36 goals scored against them). Write a program to read the file, 
 # then print the name of the team with the smallest difference in ‘for’ and ‘against’ goals.
 
+import csv
+with open('football.csv', newline = '') as csvfile:
+    reader = csv.reader(csvfile)
+    data = [row for row in reader]
+
+gd = [[row[0], abs(int(row[5])-int(row[6]))] for row in data[1:]]
+gd_min = min(gd, key = lambda x: x[1])[1]
+
+# I can't seem to figure out a more concise way to handle the scenario
+# where there is a tie for the smallest difference.
+for row in gd:
+    if row[1] == gd_min:
+        print(row[0])
